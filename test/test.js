c9b776a2125011daa9b596f67ffb6f38cb0d1a6f
siphong <iping.th@gmail.com>
2021-01-13 13:12:08 +0700

    commit

--- test/test.js
+++ test/test.js
@@ -1,8 +1,22 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+var assert = require('chai').assert;
+
+function add() {
+  return Array.prototype.slice.call(arguments).reduce(function(prev, curr) {
+    return prev + curr;
+  }, 0);
+}
+
+describe('add()', function() {
+  var tests = [
+    {args: [1, 2], expected: 3},
+    {args: [1, 2, 3], expected: 6},
+    {args: [1, 2, 3, 4], expected: 10}
+  ];
+
+  tests.forEach(function(test) {
+    it('correctly adds ' + test.args.length + ' args', function() {
+      var res = add.apply(null, test.args);
+      assert.equal(res, test.expected);
     });
   });
 });
